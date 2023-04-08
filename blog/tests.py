7b11bb5baa749ea8c92072e4fa21ca5d90f3eb1d
qwertymanuser <marlenrai14@gmail.com>
2023-04-08 18:59:30 +0600

    dz3

--- blog/tests.py
+++ blog/tests.py
@@ -1,24 +1,21 @@
-from django.test import TestCase, Client
+from django.test import TestCase,Client
 from django.urls import reverse
-
 # Create your tests here.
 class HelloViewTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-
-    def test_hello(self):
+    
+    def test_index(self):
         response = self.client.get(reverse("index-page"))
-        expected_data = "Hello"
-        self.assertEqual(expected_data, response.content.decode())
+        self.assertTemplateUsed(response,"blog/index.html")
         self.assertEqual(200, response.status_code)
-        self.assertEqual(response['name'], 'alex')  
-        
-    def test_contacts(self):
-        response = self.client.get(reverse("contacts-view"))
-        expected_data = "number"
-        self.assertEqual(expected_data, response.content.decode())
 
     def test_about(self):
         response = self.client.get(reverse("about-view"))
-        expected_data = "about"
-        self.assertEqual(expected_data, response.content.decode())
+        self.assertTemplateUsed(response,"blog/about.html")    
+        self.assertEqual(200, response.status_code)
+    
+    def test_contacts(self):
+        response = self.client.get(reverse("contacts-view"))
+        self.assertTemplateUsed(response,"blog/contacts.html")
+        self.assertEqual(200, response.status_code)
